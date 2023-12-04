e69ad3e7310c6daaf8e9a927bd1b5a432b605d63
Roger Le <roger.le@boostcommerce.net>
2023-12-04 10:49:14 +0700

    update 1

---
+++ member.js
@@ -0,0 +1,94 @@
+const MEMBERS = {
+  "h_ae_uit": {
+    "name": "Anh Em UIT +",
+    "comChieu": true 
+  },
+  "h_ae_vng": {
+    "name": "Anh Em VNG +",
+    "comChieu": true 
+  },
+  "h_ae_vinbrain": {
+    "name": "Anh Em Vinbrain +",
+    "comChieu": true 
+  },
+  "h_ae_citynow": {
+    "name": "Anh Em Citynow +",
+    "comChieu": true 
+  },
+  "h_anh_tuan": {
+    "name": "Anh Tuấn",
+    "comChieu": true 
+  },
+  "h_anh_tri": {
+    "name": "Anh Trí",
+    "comChieu": true 
+  },
+  "h_anh_triet": {
+    "name": "Anh Triết",
+    "comChieu": true 
+  },
+  "h_anh_mont": {
+    "name": "Anh Mont",
+    "comChieu": true 
+  },
+  "h_anh_peter": {
+    "name": "Anh Peter",
+    "comChieu": true 
+  },
+  "h_anh_arthur": {
+    "name": "Anh Arthur",
+    "comChieu": true 
+  },
+  "h_anh_richard": {
+    "name": "Anh Richard",
+    "comChieu": true 
+  },
+  "h_em_harry": {
+    "name": "Em Harry +",
+    "comChieu": true 
+  },
+  "h_chi_sarah": {
+    "name": "Chị Sarah",
+    "comChieu": true 
+  },
+  "h_chi_irene": {
+    "name": "Chị Irene",
+    "comChieu": true 
+  },
+  "h_chi_hani": {
+    "name": "Chị Hani",
+    "comChieu": true 
+  },
+  "h_chi_jamie": {
+    "name": "Chị Jamie +",
+    "comChieu": true 
+  },
+  "h_ban_wendy": {
+    "name": "Bạn Wendy +",
+    "comChieu": true 
+  },
+  "h_em_quinn": {
+    "name": "Em Quinn +",
+    "comChieu": true 
+  },
+  "h_ban_van": {
+    "name": "Bạn Vân +",
+    "comChieu": true 
+  },
+  "h_ban_an": {
+    "name": "Bạn An +",
+    "comChieu": true 
+  },
+  "h_ban_thai": {
+    "name": "Bạn Thái +",
+    "comChieu": true 
+  },
+  "h_ban_dong": {
+    "name": "Bạn Đồng +",
+    "comChieu": true 
+  },
+  "h_ban_minh": {
+    "name": "Bạn Minh +",
+    "comChieu": true 
+  }
+}
