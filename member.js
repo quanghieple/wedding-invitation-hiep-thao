933fefe812450a390eb67fb36c5999cac8769307
Roger Le <roger.le@boostcommerce.net>
2023-12-23 10:01:09 +0700

    update UI, member list

--- member.js
+++ member.js
@@ -1,94 +1,82 @@
 const MEMBERS = {
   "h_ae_uit": {
-    "name": "Anh Em UIT +",
-    "comChieu": true 
+    "name": "Anh Em UIT +"
+  },
+  "h_ae_dam_hoi": {
+    "name": "Anh Em Đám Hỏi",
+    "hideBank": true
   },
   "h_ae_vng": {
-    "name": "Anh Em VNG +",
-    "comChieu": true 
+    "name": "Anh Em VNG +"
   },
   "h_ae_vinbrain": {
-    "name": "Anh Em Vinbrain +",
-    "comChieu": true 
+    "name": "Anh Em Vinbrain +"
   },
   "h_ae_citynow": {
-    "name": "Anh Em Citynow +",
-    "comChieu": true 
+    "name": "Anh Em Citynow +"
   },
   "h_anh_tuan": {
     "name": "Anh Tuấn",
-    "comChieu": true 
+    "hideCC": true
   },
   "h_anh_tri": {
-    "name": "Anh Trí",
-    "comChieu": true 
+    "name": "Anh Trí"
   },
   "h_anh_triet": {
-    "name": "Anh Triết",
-    "comChieu": true 
+    "name": "Anh Triết"
   },
   "h_anh_mont": {
-    "name": "Anh Mont",
-    "comChieu": true 
+    "name": "Anh Mont"
   },
   "h_anh_peter": {
-    "name": "Anh Peter",
-    "comChieu": true 
+    "name": "Anh Peter"
   },
   "h_anh_arthur": {
-    "name": "Anh Arthur",
-    "comChieu": true 
+    "name": "Anh Arthur"
   },
   "h_anh_richard": {
-    "name": "Anh Richard",
-    "comChieu": true 
+    "name": "Anh Richard"
   },
   "h_em_harry": {
-    "name": "Em Harry +",
-    "comChieu": true 
+    "name": "Em Harry +"
   },
   "h_chi_sarah": {
-    "name": "Chị Sarah",
-    "comChieu": true 
+    "name": "Chị Sarah"
   },
   "h_chi_irene": {
-    "name": "Chị Irene",
-    "comChieu": true 
+    "name": "Chị Irene"
   },
   "h_chi_hani": {
-    "name": "Chị Hani",
-    "comChieu": true 
+    "name": "Chị Hani"
   },
   "h_chi_jamie": {
-    "name": "Chị Jamie +",
-    "comChieu": true 
+    "name": "Chị Jamie +"
   },
   "h_ban_wendy": {
-    "name": "Bạn Wendy +",
-    "comChieu": true 
+    "name": "Bạn Wendy +"
   },
   "h_em_quinn": {
-    "name": "Em Quinn +",
-    "comChieu": true 
+    "name": "Em Quinn +"
   },
   "h_ban_van": {
-    "name": "Bạn Vân +",
-    "comChieu": true 
+    "name": "Bạn Vân +"
   },
   "h_ban_an": {
-    "name": "Bạn An +",
-    "comChieu": true 
+    "name": "Bạn An +"
   },
   "h_ban_thai": {
-    "name": "Bạn Thái +",
-    "comChieu": true 
+    "name": "Bạn Thái +"
   },
   "h_ban_dong": {
-    "name": "Bạn Đồng +",
-    "comChieu": true 
+    "name": "Bạn Đồng +"
   },
   "h_ban_minh": {
-    "name": "Bạn Minh +",
-    "comChieu": true 
+    "name": "Bạn Minh +"
+  }
+}
+
+const MEMBERS_T = {
+  "t_be_ly": {
+    "name": "Bé Ly"
   }
 }
